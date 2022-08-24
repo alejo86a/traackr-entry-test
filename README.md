a5814b6d770b14ed3dc7fe179e0b7e3dd0b9197f
Alejandro Berrio <alejo86a@gmail.com>
2022-08-24 17:04:37 -0500

    documentation

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-### Build
+# Traackr entry test
+
+This is my proposed solution for the exercise. It was tested with the given file as an example and it worked fine. In addition, a script 'fill-script.js' was defined that with random strings can emulate the file up to 1GB in size. Below are the instructions for running the exercise.
+
+If you want to change the parameters for the application, in the index.js file line 8 is the path of the file to test and line 9 is the number of modified names to generate
+
+### first install the dependencies 
 `npm i`
 
 ### Execute test
-The unit tests will test that the requested formats are being accomplished without using the test file.
+the unit tests will verify that the functions that contain the algorithms work correctly
 
 `npm run test`
 
 ### Run with test file
-`node index.js`
+To run the application immediately run the following command. in the same console you will see the results of the process
+
+`npm run start`
+
+### Create a large file to test
+to create a large file to test run the following command, also don't forget to change the parameter on line 8 before running the application again
+
+`npm run fill`
--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const stream = require('stream');
 console.time('process');
 
   
-const fileName = 'coding-test-data3.txt'
+const fileName = 'files/coding-test-data.txt'
+const maxOfNames = 25   // max of names to be considered
+
+
 const instream = fs.createReadStream(fileName);
 const outstream = new stream();
 const lineReader = readline.createInterface(instream, outstream);
-let totalLines = 0;
 
 const fullNames = []; // Array of string 'lastName, name'
 const names = {}; // Dictionary of names {name: count}
 const lastNames = {}; // Dictionary of last names {last: count}
 const regExpAlphabet= /^[a-zA-Z]+$/i;
-const maxOfNames = 25   // max of names to be considered
+let totalLines = 0;
 
+// defines if the name already exists in our temporary data structure to know if it increases the occurrence counter or starts it
 const insertIn = (arr, key)=>{
     let value = arr[key];
     if (typeof value!=='undefined') {
@@ -25,24 +28,25 @@ const insertIn = (arr, key)=>{
         return arr[key]=1;
     }
 }
+
+// defines through a regular expression if a full name is valid or not to be processed
 const isValidFullName = nameSplited => !regExpAlphabet.test(nameSplited[0]) || !regExpAlphabet.test(nameSplited[1]);
 
+// count the number of keys in our data structure to find out the number of elements. By the way it is agnostic to whether it is the array of names or surnames
 const countBy = (arr) =>{
     return Object.keys(arr).length
 }
 
-const countByFullName= ()=>{
-    return 1
-}  
-
+// returns the first 10 elements with the most occurrences. By the way it is agnostic to whether it is the array of names or surnames
 const orderBy=(arr)=>{
     return Object.entries(arr).sort((a,b)=>b[1]-a[1]).slice(0,10).map(x=>x[0]+' with '+x[1])
 }
 
-const getStep1=()=>{
+// iterates through the above data structure to find the first occurrence of each first and last name by inserting the full name without repeats into the resulting array up to the given limit
+const getStep1=(arr)=>{
     const a = [];
-    for (let i = 0; i < fullNames.length; i++) {
-        const auxNameSplited = fullNames[i].split(', ');
+    for (let i = 0; i < arr.length; i++) {
+        const auxNameSplited = arr[i].split(', ');
         if(a.length===maxOfNames) break;
         if(a.findIndex(x=>x.name===auxNameSplited[1])===-1 && a.findIndex(x=>x.lastName===auxNameSplited[0])===-1){
             a.push({name: auxNameSplited[1], lastName: auxNameSplited[0]})
@@ -51,13 +55,15 @@ const getStep1=()=>{
     return a;
 }
 
+// in the given array it keeps the position of the surnames and makes a slight displacement of the names to generate a completely new array of full names
 const getStep2=(step1)=>{
     if(step1.length<2) return 'It is not possible to create a step 2';
     return step1.map((x,i)=>{
         return {name: i===(step1.length-1)?step1[0].name:step1[(i+1)].name, lastName: x.lastName}
     });
 }
-  
+
+// reads the file line by line, checks if that line is valid, formats it and then stores it in our temporary data structure
 lineReader.on('line', line => {
     totalLines = (totalLines + 1)%2;
     if(totalLines===1){
@@ -77,16 +83,25 @@ lineReader.on('line', line => {
         names[firstName] = insertIn(names, firstName);
 
     }
-    
-    
+});
 
-  });  
-  lineReader.on('close', () => {
+// From the data stored in our data structure, it calls the functions that process them and continues printing the information required by the test in the console.
+lineReader.on('close', () => {
     console.log('Cardinality by name: ' + countBy(names)+ ' Cardinality by lastName: ' + countBy(lastNames) +' Cardinality by full name: ' + fullNames.length);
     console.log('The ten most common last names sorted in descending order '+ orderBy(lastNames).join(', '));
     console.log('The ten most common first names sorted in descending order '+ orderBy(names).join(', '));
-    const step1 = getStep1();
+    const step1 = getStep1(fullNames);
     console.log('step1',step1)
     console.log('step2',getStep2(step1))
     console.timeEnd('process')
-  });
+});
+
+// export the functions of the application so that they can be tested in another file
+module.exports = {
+    insertIn,
+    isValidFullName,
+    countBy,
+    orderBy,
+    getStep1,
+    getStep2
+};
